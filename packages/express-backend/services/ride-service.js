2f63d95f080992a9b4ba1a748e26df6ff16cd93c
Danny-Kullman <dannykullman@gmail.com>
2026-02-09 14:05:30 -0800

    added get/delete ride services

--- packages/express-backend/services/ride-service.js
+++ packages/express-backend/services/ride-service.js
@@ -14,6 +14,30 @@ mongoose.set('debug', true);
 //     })
 // }
 
+//function to get rides going to that destination regardless of date
+function getRides(destination) {
+  const promise = rideModel.find({ destination: destination}).catch((err) => console.log(err));
+  return promise;
+}
+
+//function to get rides going to that destination on that date
+function getRidesByDate(destination, date) {
+  const promise = rideModel.find({ destination: destination, date: date }).catch((err) => console.log(err));
+  return promise;
+}
+
+//function to get a ride by its id
+function getRideById(rideId) {
+  const promise = rideModel.findById(rideId).catch((err) => console.log(err));
+  return promise;
+}
+
+//function to delete a ride by its id
+function deleteRide(rideId) {
+  const promise = rideModel.findByIdAndDelete(rideId).catch((err) => console.log(err));
+  return promise;
+}
+
 // function to create a ride in the database
 function createRide(ride) {
   const rideToAdd = new rideModel(ride);
